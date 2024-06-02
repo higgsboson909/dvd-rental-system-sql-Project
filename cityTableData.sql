d9b0041d89bf82d0140c92c266c15470e6359c91
higgsboson909 <higgsbosson906@gmail.com>
2024-06-03 00:43:30 +0500

    city and country table updated

---
+++ cityTableData.sql
@@ -0,0 +1,17 @@
+insert into city(city_id, city_name, country_id) 
+values
+(1,'Islamabad',1),
+(2,'Sialkot',1),
+(3,'Faisalabad',1),
+(4,'Lahore',1),
+(5,'Delhi',2),
+(6,'Mumbai',2),
+(7,'Quetta',1),
+(8,'Chennai',2),
+(9,'Hyderabad',2),
+(10,'Bhopaal',2),
+(11,'Istunbul',7),
+(12,'Dhaaka',3),
+(13,'Paris',6),
+(14,'London',5),
+(15,'Colombo',4);
---
+++ countryTableData.sql
@@ -0,0 +1,8 @@
+insert into country values
+(1,'Pakistan'),
+(2,'India'),
+(3,'Bangladesh'),
+(4,'Sirlanka'),
+(5,'England'),
+(6,'France'),
+(7,'Turkey');
